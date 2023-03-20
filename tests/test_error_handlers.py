8ce583f62dbede1b0960b27bbf2ada27df546304
nzorro1978 <nzojihipolito@gmail.com>
2023-03-19 21:21:41 -0400

    formatted test_errors for flake8

--- tests/test_error_handlers.py
+++ tests/test_error_handlers.py
@@ -14,13 +14,13 @@ class TestErrorHandlers(TestCase):
     def test_not_found(self):
         """Test handling of HTTP_404"""
         status_code = eh.status.HTTP_404_NOT_FOUND
-        
+
         res, status = eh.not_found(status_code)
         self.assertEqual(status, status_code)
         self.assertEqual(res.get_json()['status'], status_code)
         self.assertGreater(len(res.get_json()['error']), 0)
         self.assertGreater(len(res.get_json()['message']), 0)
-   
+
     def test_method_not_supported(self):
         """Tests handling of method not supported"""
         status_code = eh.status.HTTP_405_METHOD_NOT_ALLOWED
@@ -33,8 +33,8 @@ class TestErrorHandlers(TestCase):
     def test_internal_server_error(self):
         """Tests handling of internal server error"""
         status_code = eh.status.HTTP_500_INTERNAL_SERVER_ERROR
-        res,status = eh.internal_server_error(status_code)
+        res, status = eh.internal_server_error(status_code)
         self.assertEqual(status, status_code)
         self.assertEqual(res.get_json()['status'], status_code)
-        self.assertGreater(len(res.get_json()['error']),0)
-        self.assertGreater(len(res.get_json()['message']),0)
+        self.assertGreater(len(res.get_json()['error']), 0)
+        self.assertGreater(len(res.get_json()['message']), 0)
